b4f80a7cfe0e675826a38b9d26b720406538db47
Dmitriy <jekas213@gmail.com>
2023-03-23 14:23:16 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean check = check("Da2_2hf", "fF2hg_jvjhvjhv", "fF2hg_jvjhvjhv");
+        System.out.println(check);
+
     }
+
+    public static boolean checkLoginAndPassword(String login) {
+        if (login.matches("\\w+")) {
+            return true;
+        } else return false;
+    }
+
+    public static boolean check(String login, String password, String confirmPassword) {
+        try {
+            if (login.length() > 20) {
+                throw new WrongLoginException();
+            }
+            if (!password.equals(confirmPassword)) {
+                throw new WrongPasswordException();
+            }
+        } catch (WrongLoginException e) {
+            System.out.println("Логин больше 20 символов");
+            return false;
+        } catch (WrongPasswordException q) {
+            System.out.println("Пароли не совпадают");
+            return false;
+        }
+        if (password.length() >= 20) {
+            return false;
+        }
+        if (checkLoginAndPassword(login) && checkLoginAndPassword(password)) {
+            return true;
+        } else return false;
+    }
+
 }
